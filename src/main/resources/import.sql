4cda75b48cf808ee3472f496f8b0ef113fe0a7e1
sebasjaga <sebastian.jaramillo03@uceva.edu.co>
2023-10-18 22:25:50 -0500

    Se agrego parte del nuevo metodo
    Se listó en la tabla de pgAdmin la cantidad de computadores de cada sala

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO Salas (id,Salas) VALUES (271,'SALA A');
-INSERT INTO Salas (id,Salas) VALUES (194,'SALA B');
-INSERT INTO Salas (id,Salas) VALUES (222,'SALA C');
-INSERT INTO Salas (id,Salas) VALUES (227,'SALA D');
-INSERT INTO Salas (id,Salas) VALUES (274,'SALA E');
-INSERT INTO Salas (id,Salas) VALUES (259,'SALA F');
-INSERT INTO Salas (id,Salas) VALUES (260,'SALA G');
+INSERT INTO Salas (id,Salas, cant) VALUES (271,'SALA A', 'cantidad de 15 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (194,'SALA B', 'cantidad de 20 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (222,'SALA C', 'cantidad de 18 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (227,'SALA D', 'cantidad de 22 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (274,'SALA E', 'cantidad de 10 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (259,'SALA F', 'cantidad de 12 computadores');
+INSERT INTO Salas (id,Salas, cant) VALUES (260,'SALA G', 'cantidad de 5 computadores');
 
